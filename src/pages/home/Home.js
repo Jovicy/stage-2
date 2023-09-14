f39c276d292ccf63b6877d1bae619ffea4d8aa08
unknown <johnvictordml@gmail.com>
2023-09-14 06:46:20 +0100

    resolving issues before deploying

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Home = () => {
+
+    useEffect(() => {
+        fetch("https://api.themoviedb.org/3/movie/popular?api_key=2c32c5748ed591a2830d0caf372626aa&language=en-US")
+        .then(res => res.json())
+        .then(data  => console.log(data))
+    }, [])
+
   return (
-    <div>Home</div>
+    <div>
+        home page is rendered
+    </div>
   )
 }
 
