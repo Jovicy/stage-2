451439b6d0a5427da77997ace4d5870020521a55
unknown <johnvictordml@gmail.com>
2023-09-16 18:19:43 +0100

    stil changing path

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import Movie from './pages/movie details/Movie';
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Router>
         <Routes>
           <Route path="/" element={<Header />} />
-
-          {/* Define the route for movie details */}
           <Route path="movie/:id" element={<Movie />} />
         </Routes>
       </Router>
