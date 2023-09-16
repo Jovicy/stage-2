280e113451efec6961d1560207efdfa3cb74de88
unknown <johnvictordml@gmail.com>
2023-09-16 17:56:23 +0100

    changed the path

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Movie from './pages/movie details/Movie';
 
-
 export default function App() {
   return (
     <div className="App">
       <Router>
-        <Header/>
         <Routes>
-          <Route exact element={<Header/>}></Route>
-          <Route path="movie/:id" element={<Movie/>}></Route>
+          <Route path="/" element={<Header />} />
+
+          {/* Define the route for movie details */}
+          <Route path="movie/:id" element={<Movie />} />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
